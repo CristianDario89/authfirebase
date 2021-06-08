f33425a9e5d203887304ae42d3e5fd4c608b3494
Cristiano10 <ccristiandarioromero@gmail.com>
2021-06-08 03:19:08 -0300

    init

---
+++ main.js
@@ -0,0 +1,137 @@
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
+
+// SignUp
+const signUpForm = document.querySelector("#signup-form");
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signUpForm["signup-email"].value;
+  const password = signUpForm["signup-password"].value;
+
+  // Authenticate the User
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // clear the form
+      signUpForm.reset();
+      // close the modal
+      $("#signupModal").modal("hide");
+    });
+});
+
+// Logout
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("signup out");
+  });
+});
+
+// SingIn
+const signInForm = document.querySelector("#login-form");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signInForm["login-email"].value;
+  const password = signInForm["login-password"].value;
+
+  // Authenticate the User
+  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // clear the form
+    signInForm.reset();
+    // close the modal
+    $("#signinModal").modal("hide");
+  });
+});
+
+// Posts
+const postList = document.querySelector(".posts");
+const setupPosts = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => { //x cada doc dentro de la lista
+      const post = doc.data(); //para obtener los d dentro d ese doc
+      const li = `
+      <li class="list-group-item list-group-item-action">
+        <h5>${post.title}</h5>
+        <p>${post.descripcion}</p>
+      </li>
+    `;
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
+  }
+};
+
+// events
+// list for auth state changes
+auth.onAuthStateChanged((user) => { // se dispara en cada auth,
+  // un usu se loguea se dispara este ev, sale de la app se dispara
+  //asi q cada vez q se dispara obten el obj user
+
+  if (user) { //si existe esta logueado
+    console.log("signin");
+    fs.collection("posts") //consultar coleccion
+      .get()//met pa traer esos D
+      .then((snapshot) => { //ya obtenidos quiero q me muestre el est actual d esos D = un snapshot
+        //un obj q me muestra los datos q tengo en la coleccion xq pueden ir cambiando
+        setupPosts(snapshot.docs);
+        loginCheck(user);
+      });
+  } else {
+    console.log("signout");
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
+
+// Login with Google
+const googleButton = document.querySelector("#googleLogin");
+
+googleButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  signInForm.reset();
+  $("#signinModal").modal("hide");
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("google sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
+
+// Login with Facebook
+const facebookButton = document.querySelector('#facebookLogin');
+
+facebookButton.addEventListener('click', e => {
+  e.preventDefault();
+  signInForm.reset();
+  $("#signinModal").modal("hide");
+
+  const provider = new firebase.auth.FacebookAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("facebook sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+})
